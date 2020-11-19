05c2c567ca9671a0091803f98c8f7a6fb0b8e0e2
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2020-11-19 13:08:49 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 tempF=60
-while True: 
+while True:
+    print ("tempature"+ input.temperature(TemperatureUnit.FAHRENHEIT))  
     if input.temperature(TemperatureUnit.FAHRENHEIT) > tempF:
         light.set_pixel_color(5, light.rgb(255, 0, 0))
     else : 
--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 let tempF = 60
 while (true) {
+    console.log("tempature" + input.temperature(TemperatureUnit.Fahrenheit))
     if (input.temperature(TemperatureUnit.Fahrenheit) > tempF) {
         light.setPixelColor(5, light.rgb(255, 0, 0))
     } else {
