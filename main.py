7d62dda4e1817c5a0ff66d7ae1378fef160d3f47
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2020-11-23 19:45:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ while True:
     if input.temperature(TemperatureUnit.FAHRENHEIT) > tempF:
         light.set_pixel_color(5, light.rgb(255, 0, 0))
     else:  
-        light.clear()
+        light.clear() 
